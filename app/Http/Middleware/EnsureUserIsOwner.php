2f8e78d5a0af164cfa4e74ca82029938e5d3108b
olatisunkanmi <igbasanolasunkanmi080@gmail.com>
2023-07-14 01:20:56 +0100

    fix: protect user resources with middleware

--- app/Http/Middleware/EnsureUserIsOwner.php
+++ app/Http/Middleware/EnsureUserIsOwner.php
@@ -8,7 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Symfony\Component\HttpFoundation\Response;
 
-class EnsureOwnership
+class EnsureUserIsOwner
 {
     /**
      * Handle an incoming request.
